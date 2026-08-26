b24dcc2bcd3d0cbc862fb0b5ddf0d306b1b25ee4
Shubham Banerjee <shubhoinweb@gmail.com>
2026-08-27 00:38:19 +0530

    Update app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Breast Cancer Transformer"
+)
+
+st.title("🩺 Breast Cancer Prediction")
+st.success("Streamlit is working successfully!")
+
+st.write("This is a deployment test.")
